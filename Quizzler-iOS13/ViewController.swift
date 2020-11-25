ccacc7b220f1fe90a1dcba9c8b274c6fe4116689
Yani Buchkov <ybuchkov@tfxcorp.com>
2020-11-25 11:39:36 +0200

    104. Checking Answers using 2-Dimensional Arrays

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet private weak var falseButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is greater than One.",
-        "Three + Eight is less than Ten"
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One.", "True"],
+        ["Three + Eight is less than Ten", "False"]
     ]
     
     var questionNumber = 0
@@ -39,12 +39,26 @@ class ViewController: UIViewController {
     
     @IBAction
     private func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        
+        let userAnswer = sender.currentTitle // True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer  {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
         updateUI()
     }
     
     private func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
 }
 
